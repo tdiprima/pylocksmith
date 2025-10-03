d253458f6aa42369137a915a972c8b825976d337
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-10-03 10:59:06 -0400

    reorganized

--- run_security_lint.sh
+++
@@ -1,5 +0,0 @@
-# This script installs bandit and runs it to lint your Python project for security issues,
-# such as weak hashing or potential injection vulnerabilities.
-
-pip install bandit
-bandit -r path/to/your_project/
---
+++ src/pylocksmith/run_security_lint.sh
@@ -0,0 +1,4 @@
+# This script installs bandit and runs it to lint your Python project for security issues
+
+pip install bandit
+bandit -r .
